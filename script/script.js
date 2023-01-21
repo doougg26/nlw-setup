b96c293adb88dc1473d11bcdd189be1a178373da
douglas <dcorrea566@gmail.com>
2023-01-21 17:30:03 -0300

    initial commit

---
+++ script/script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+
+const button =document.querySelector('header button')
+button.addEventListener('click',add)
+form.addEventListener('change', save)
+
+function add(){
+    const today = new Date().toLocaleDateString('pt-br').slice(0,-5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    
+if(dayExists){
+alert("dia já incluso")
+return
+} 
+nlwSetup.addDay(today)
+}
+function save(){
+    localStorage.setItem('nlw@habits',JSON.stringify(nlwSetup.data))
+}
+/*const data ={
+    run:["01-01", "01-02", "01-06", "01-07", "01-08"],
+    water:["01-03"],
+    food:["01-02"]
+}
+*/
+const data = JSON.parse(localStorage.getItem("nlw@habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
